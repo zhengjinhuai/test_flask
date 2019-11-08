6f57022ae8f756284db56bcdee2a814ebc40b362
zhengjinhuai <873217817@qq.com>
2019-11-08 19:43:41 +0800

    8.7.3 修改电子邮件地址 有bug

--- app/email.py
+++ app/email.py
@@ -3,10 +3,18 @@ from flask import current_app, render_template
 from flask_mail import Message
 from . import mail
 
+"""
+    问题：此处为什么不直接使用current_app呢？:
+    解答：Return the current object. This is useful if you want the real object behind the proxy at a time
+        for performance reasons or because you want to pass the object into a different context.
+        返回当前对象。如果出于性能原因或希望将对象传递到其他上下文中，一次需要代理后面的实例，则这非常有用。
+
+    发邮件优化代码：https://www.cnblogs.com/santiego/p/10356001.html
+"""
+
 
 def send_async_email(app, msg):
     with app.app_context():
-        print('已发送！')
         mail.send(msg)
 
 
@@ -19,15 +27,3 @@ def send_email(to, subject, template, **kwargs):
     thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
-
-
-
-"""
-    问题：此处为什么不直接使用current_app呢？:
-    解答：Return the current object. This is useful if you want the real object behind the proxy at a time
-        for performance reasons or because you want to pass the object into a different context.
-        返回当前对象。如果出于性能原因或希望将对象传递到其他上下文中，一次需要代理后面的实例，则这非常有用。
-
-    发邮件优化代码：https://www.cnblogs.com/santiego/p/10356001.html
-"""
-
