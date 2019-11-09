026a9b79c6287f50586895a6baacebed91fbbf1e
zhengjinhuai <873217817@qq.com>
2019-11-09 21:45:04 +0800

    10.5 添加散列值字段

--- app/models.py
+++ app/models.py
@@ -87,6 +87,7 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)  # 用户每次访问网站后都要刷新时间
+    avatar_hash = db.Column(db.String(32))  # gavatar头像的MD5散列值
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)  # 这里用**kwargs将关键字参数传递给父类
@@ -95,6 +96,8 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = self.gravatar_hash()
 
 
     @property
@@ -163,6 +166,7 @@ class User(UserMixin, db.Model):
         if self.query.filter_by(email=new_email).first() is not None:
             return False
         self.email = new_email
+        self.avatar_hash = self.gravatar_hash()
         db.session.add(self)
         return True
 
@@ -175,6 +179,9 @@ class User(UserMixin, db.Model):
 
     def is_administrator(self):
         return self.can(Permission.ADMIN)
+
+    def gravatar_hash(self):
+        return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
 
     def gravatar(self, size=100, default='identicon', rating='g'):
         url = 'https://secure.gravatar.com/avatar'
